7577503d97cf94a89df0e0ea44dc9635dcfd95bd
etnq <baruasayantan21@gmail.com>
2026-08-18 12:43:45 +0530

    Fix formatting and spacing in app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import os
+
 class InstagramScript:
     def __init__(self):
-        self.items=["Login", "Exit", "About Page"]
+        self.items = ["Login", "Exit", "About Page"]
+
     def clear_terminal(self):
-        os.system('cls' if os.name =='nt' 
-else 'clear')
-        
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def display_list(self):
         print("\n----Content---")
-        for index, item in 
-enumerate(self.items, start=1):
+        for index, item in enumerate(self.items, start=1):
             print(f"{index}. {item}")
-print("------------------------------\n")
-app=InstagramScript()
-app.clear_terminal()
-print("Instagram Script")
-app.display_list()
+        print("------------------------------\n")
+
+if __name__ == "__main__":
+    app = InstagramScript()
+    app.clear_terminal()
+    print("Instagram Script")
+    app.display_list()
