a2d286dd8ba6c512ad191416175325dc02ede79a
etnq <baruasayantan21@gmail.com>
2026-08-18 12:40:42 +0530

    Fix formatting in display_list method

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ class InstagramScript:
     def clear_terminal(self):
         os.system('cls' if os.name =='nt' 
 else 'clear')
+        
     def display_list(self):
         print("\n----Content---")
         for index, item in 
 enumerate(self.items, start=1):
-    print(f"{index}.{item}")
+            print(f"{index}. {item}")
 print("------------------------------\n")
 app=InstagramScript()
 app.clear_terminal()
